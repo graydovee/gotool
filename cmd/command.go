6c60376afab2ebff1b617e9725f6a78a6ed811fe
dujiahui <dujiahui@agora.io>
2021-09-17 17:42:39 +0800

    add command

--- cmd/command.go
+++ cmd/command.go
@@ -14,7 +14,9 @@ func init() {
 	rootCmd.AddCommand(
 		NewSearchAliveAddrCommand(), // 搜索网段内，所有能ping通的机器
 		NewServerSearchCommand(),    // 搜索网段内，所有开启22端口的机器（服务器）
-		NewTcpScannerCommand(),      //扫描开启tcp监听的端口
+		NewTcpScannerCommand(),      // 扫描开启tcp监听的端口
+		NewIpToIntCommand(), 		 // ip字符串转int32
+		NewUUIDCommand(), 			 // uuid 生成器
 	)
 }
 
